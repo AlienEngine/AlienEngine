16a68342d79ecf4e36f1da98f73a501ecdab5422
Nana Axel <ax.lnana@outlook.com>
2017-08-06 16:54:56 +0100

    Remove unused using statements

--- AlienEngine/Game/Components/MeshRenderer.cs
+++ AlienEngine/Game/Components/MeshRenderer.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AlienEngine.Core.Game;
 using AlienEngine.Graphics;
-using AlienEngine.Graphics.Shaders;
-using AlienEngine.Core.Game;
 
 namespace AlienEngine
 {
