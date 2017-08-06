a87c40a87706b0b469ff5be67844d179ff7f4642
Nana Axel <ax.lnana@outlook.com>
2017-08-06 16:49:04 +0100

    Add Light component (Used to add lights of various types in the game)

--- AlienEngine/Game/Components/Light.cs
+++ AlienEngine/Game/Components/Light.cs
@@ -6,15 +6,78 @@ using System.Threading.Tasks;
 
 namespace AlienEngine
 {
+    /// <summary>
+    /// Light Component.
+    /// </summary>
     public class Light : Component
     {
-        public enum LightType
+        /// <summary>
+        /// The type of this light.
+        /// </summary>
+        public LightType Type;
+
+        /// <summary>
+        /// The ambient light color.
+        /// </summary>
+        public Color4 AmbientColor;
+
+        /// <summary>
+        /// The diffuse color.
+        /// </summary>
+        public Color4 DiffuseColor;
+
+        /// <summary>
+        /// The specular color.
+        /// </summary>
+        public Color4 SpecularColor;
+
+        /// <summary>
+        /// The ambient intensity of this light.
+        /// </summary>
+        public float Intensity;
+
+        /// <summary>
+        /// The light direction.
+        /// </summary>
+        /// <remarks>
+        /// This is used by <see cref="LightType.Directional"/> and <see cref="LightType.Spot"/> lights only.
+        /// </remarks>
+        public Vector3f Direction
         {
-            Spot,
-            Directional,
-            Sun
+            get
+            {
+                var mat = Matrix4f.CreateRotation(gameElement.WorldTransform.Rotation);
+                return mat.Column2.XYZ;
+            }
         }
 
-        public LightType Type;
+        /// <summary>
+        /// Attenuation factors of the current light.
+        /// Set the <see cref="Vector3f.X"/> component to set the constant attenuation.
+        /// Set the <see cref="Vector3f.Y"/> component to set the linear attenuation.
+        /// Set the <see cref="Vector3f.Z"/> component to set the quadratic attenuation.
+        /// </summary>
+        /// <remarks>
+        /// This is used by <see cref="LightType.Point"/> and <see cref="LightType.Spot"/> lights only.
+        /// </remarks>
+        public Vector3f AttenuationFactors;
+
+        /// <summary>
+        /// Light fall-off exponent.
+        /// </summary>
+        /// <remarks>
+        /// This is used by <see cref="LightType.Spot"/> lights only.
+        /// </remarks>
+        public float FallOffExponent;
+
+        /// <summary>
+        /// Light fall-off angles.
+        /// Set the <see cref="Vector2f.X"/> component to set the inner angle (in degrees).
+        /// Set the <see cref="Vector2f.Y"/> component to set the outer angle (in degrees).
+        /// </summary>
+        /// <remarks>
+        /// This is used by <see cref="LightType.Spot"/> lights only.
+        /// </remarks>
+        public Vector2f FallOffAngles;
     }
 }
