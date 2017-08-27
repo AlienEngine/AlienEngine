b3fdda93548507c1cb621f4a08adb923c167c903
Nana Axel <ax.lnana@outlook.com>
2017-08-27 01:33:13 +0100

    Update implementation of components

--- AlienEngine/Game/Component.cs
+++ AlienEngine/Game/Component.cs
@@ -1,35 +1,141 @@
 ﻿namespace AlienEngine
 {
+    /// <summary>
+    /// Base class of all built-in and user defined
+    /// game components.
+    /// </summary>
     public abstract class Component : IComponent
     {
         private bool _enabled = true;
         private GameElement _gameElement = null;
 
+        /// <summary>
+        /// The <see cref="GameElement"/> in which
+        /// this <see cref="Component"/> is attached.
+        /// </summary>
         protected GameElement gameElement
         {
             get { return _gameElement; }
         }
 
-        public bool Enabled { get { return _enabled; } }
+        /// <summary>
+        /// Gets a boolean defining if this <see cref="Component"/>.
+        /// is enabled or not.
+        /// </summary>
+        protected bool Enabled { get { return _enabled; } }
 
-        public void Enable()
+        /// <summary>
+        /// Enable this <see cref="Component"/>.
+        /// </summary>
+        protected void Enable()
         {
             Enable(true);
         }
 
-        public void Disable()
+        /// <summary>
+        /// Disable this <see cref="Component"/>.
+        /// </summary>
+        protected void Disable()
         {
             Enable(false);
         }
 
-        public void Enable(bool value)
+        /// <summary>
+        /// Enable or disable this <see cref="Component"/>.
+        /// </summary>
+        /// <param name="value">true to enable, false otherwise.</param>
+        protected void Enable(bool value)
         {
             _enabled = value;
         }
 
-        public T GetComponent<T>() where T : Component
+        /// <summary>
+        /// Enable all components of type <typeparamref name="T"/>
+        /// in this <see cref="GameElement"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/> to enable.</typeparam>
+        protected void EnableAll<T>() where T : Component
+        {
+            var collection = gameElement.GetComponents<T>();
+            foreach (var component in collection)
+            {
+                component.Enable();
+            }
+        }
+
+        /// <summary>
+        /// Disable all components of type <typeparamref name="T"/>
+        /// in this <see cref="GameElement"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/> to enable.</typeparam>
+        protected void DisableAll<T>() where T : Component
+        {
+            var collection = gameElement.GetComponents<T>();
+            foreach (var component in collection)
+            {
+                component.Disable();
+            }
+        }
+
+        /// <summary>
+        /// Enable or disable all components of type <typeparamref name="T"/>
+        /// in this <see cref="GameElement"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/> to enable.</typeparam>
+        /// <param name="value">true to enable, false otherwise.</param>
+        protected void EnableAll<T>(bool value) where T : Component
+        {
+            var collection = gameElement.GetComponents<T>();
+            foreach (var component in collection)
+            {
+                component.Enable(value);
+            }
+        }
+
+        /// <summary>
+        /// Gets and returns the instance of the <see cref="Component"/>
+        /// of type <typeparamref name="T"/> attached to this <see cref="GameElement"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/> to return.</typeparam>
+        /// <remarks>
+        /// In the case where more than one component of the type <typeparamref name="T"/>
+        /// are attached to this game element, the first one will be returned.
+        /// </remarks>
+        protected T GetComponent<T>() where T : Component
         {
             return _gameElement != null ? _gameElement.GetComponent<T>() : null;
+        }
+
+        /// <summary>
+        /// Gets and returns the instance of all <see cref="Component"/>s
+        /// of type <typeparamref name="T"/> attached to this <see cref="GameElement"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/> to return.</typeparam>
+        protected T[] GetComponents<T>() where T : Component
+        {
+            return _gameElement != null ? _gameElement.GetComponents<T>() : null;
+        }
+
+        /// <summary>
+        /// Checks if the current <see cref="GameElement"/> has an
+        /// attached <see cref="Component"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/>.</typeparam>
+        protected bool HasComponent<T>() where T : Component
+        {
+            return _gameElement != null ? _gameElement.HasComponent<T>() : false;
+        }
+
+        /// <summary>
+        /// Checks if the current <see cref="GameElement"/> has an
+        /// attached <see cref="Component"/> of type <typeparamref name="T"/>
+        /// with the specified instance.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Component"/>.</typeparam>
+        /// <param name="component">The instance of <typeparamref name="T"/> to find.</param>
+        protected bool HasComponent<T>(T component) where T : Component
+        {
+            return _gameElement != null ? _gameElement.HasComponent(component) : false;
         }
 
         internal void SetGameElement(GameElement gameElement)
@@ -52,6 +158,18 @@
         { }
 
         public virtual void Stop()
+        { }
+
+        public virtual void OnColliderHit(Collider element)
+        { }
+
+        public virtual void OnColliderEnter(Collider element)
+        { }
+
+        public virtual void OnColliderStay(Collider element)
+        { }
+
+        public virtual void OnColliderExit(Collider element)
         { }
 
         #endregion
--- AlienEngine/Game/IComponent.cs
+++ AlienEngine/Game/IComponent.cs
@@ -8,14 +8,68 @@ namespace AlienEngine
 {
     internal interface IComponent
     {
+        #region Component behavior
+
+        /// <summary>
+        /// Triggered when the <see cref="Game"/> is starting.
+        /// Initialize your component with this method.
+        /// </summary>
         void Start();
 
+        /// <summary>
+        /// Trigerred just before the frame update.
+        /// </summary>
         void BeforeUpdate();
 
+        /// <summary>
+        /// Triggered during the frame update.
+        /// </summary>
         void Update();
 
+        /// <summary>
+        /// Trigerred just after the frame update.
+        /// </summary>
         void AfterUpdate();
 
+        /// <summary>
+        /// Trigerred when the <see cref="Game"/> is being stopped.
+        /// You can use this method to release resources used by your
+        /// component.
+        /// </summary>
         void Stop();
+
+        #endregion
+
+        #region Physics triggers
+
+        /// <summary>
+        /// Trigerred when the <see cref="Collider"/> of this
+        /// <see cref="GameElement"/> hit another.
+        /// </summary>
+        /// <param name="element">The other collider.</param>
+        void OnColliderHit(Collider element);
+
+        /// <summary>
+        /// Trigerred when the <see cref="Collider"/> of this
+        /// <see cref="GameElement"/> enter in another.
+        /// </summary>
+        /// <param name="element">The other collider.</param>
+        void OnColliderEnter(Collider element);
+
+        /// <summary>
+        /// Trigerred when the <see cref="Collider"/> of this
+        /// <see cref="GameElement"/> is in another.
+        /// </summary>
+        /// <param name="element">The other collider.</param>
+        void OnColliderStay(Collider element);
+
+        /// <summary>
+        /// Trigerred when the <see cref="Collider"/> of this
+        /// <see cref="GameElement"/> exit out another.
+        /// </summary>
+        /// <param name="element">The other collider.</param>
+        void OnColliderExit(Collider element);
+
+        #endregion
     }
 }
