95d05ba3fa4d64ab0ee9c824f632bc702b255c3c
Nana Axel <ax.lnana@outlook.com>
2017-08-27 01:07:07 +0100

    Update AssemblyInfo

--- AlienEngine/Properties/AssemblyInfo.cs
+++ AlienEngine/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +7,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AlienEngine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# game library better than others.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AlienGames")]
 [assembly: AssemblyProduct("AlienEngine")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
