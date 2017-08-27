0f88dab2ceaa8210b5d415e24e45a12dda35e9af
Nana Axel <ax.lnana@outlook.com>
2017-08-27 01:04:38 +0100

    Better management of the engine

--- AlienEngine/Engine.cs
+++ AlienEngine/Engine.cs
@@ -1,45 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AlienEngine.Core.Audio;
-using AlienEngine.Core.Audio.OpenAL;
-using AlienEngine.Core.Graphics.DevIL;
+﻿using AlienEngine.Core.Audio;
+using AlienEngine.Core.Graphics.DevIL.Unmanaged;
 using AlienEngine.Core.Graphics.OpenGL;
 using AlienEngine.Core.Resources;
 
 namespace AlienEngine.Core
 {
+    /// <summary>
+    /// AlienEngine Manager.
+    /// </summary>
     public static class Engine
     {
+        private static bool _started;
         private static AudioContext _audioContext;
 
-        public static void Start()
+        /// <summary>
+        /// Checks if the engine has already started.
+        /// </summary>
+        public static bool HasStarted
         {
-            // Initialize OpenGL
-            GL.LoadOpenGL();
-
-            // Initialize DevIL
-            IL.Init();
-            ILU.Init();
-            ILUT.Init();
-
-            // Set DevIL default renderer
-            ILUT.Renderer(ILUT.ILUT_OPENGL);
-
-            // Initialize OpenAL and EFX
-            _audioContext = new AudioContext();
-            if (!_audioContext.IsCurrent)
-                _audioContext.MakeCurrent();
+            get { return _started; }
         }
 
+        /// <summary>
+        /// Initialize the manager.
+        /// </summary>
+        static Engine()
+        {
+            _started = false;
+        }
+
+        /// <summary>
+        /// Start the engine. Load common libraries and
+        /// initialize graphics and audio contexts.
+        /// </summary>
+        public static void Start()
+        {
+            if (!_started)
+            {
+                // --------------------
+                // OpenGL
+                // --------------------
+                // Initialize OpenGL
+                GL.LoadOpenGL();
+                // --------------------
+
+                // --------------------
+                // DevIL
+                // --------------------
+                // Initialize DevIL
+                IL.Initialize();
+                ILU.Initialize();
+                // --------------------
+
+                // --------------------
+                // OpenAL
+                // --------------------
+                // Initialize OpenAL and EFX
+                _audioContext = new AudioContext();
+                if (!_audioContext.IsCurrent)
+                    _audioContext.MakeCurrent();
+                // --------------------
+
+                _started = true;
+            }
+        }
+
+        /// <summary>
+        /// Stop the engine. Free all managed and unmanaged resources
+        /// used during the game.
+        /// </summary>
         public static void Stop()
         {
-            // Dispose OpenAL
-            _audioContext.Dispose();
+            // --------------------
+            // DevIL
+            // --------------------
+            // Shutdown DevIL
+            IL.Shutdown();
+            // --------------------
 
-            // Dispose all other resources
+            // Dispose all used (managed or unmanaged) resources
             ResourcesManager.DisposeResources();
         }
     }
