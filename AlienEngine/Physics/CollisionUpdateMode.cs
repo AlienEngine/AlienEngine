3da0a76df0e2e905f3444c678de6adc4ff1af54b
Nana Axel <ax.lnana@outlook.com>
2017-09-16 01:46:38 +0100

    Add wrapper for inner position update mode enumeration

---
+++ AlienEngine/Physics/CollisionUpdateMode.cs
@@ -0,0 +1,11 @@
+﻿using BEPUphysics.PositionUpdating;
+
+namespace AlienEngine.Core.Physics
+{
+    public enum CollisionUpdateMode
+    {
+        Continuous = PositionUpdateMode.Continuous,
+        Discrete = PositionUpdateMode.Discrete,
+        Passive = PositionUpdateMode.Passive
+    }
+}
