28ac0c70491f0195c64751714870f5435c2e43f3
Nana Axel <ax.lnana@outlook.com>
2017-09-16 02:05:41 +0100

    Update library configurations and packages

---
+++ AlienEngine/Application.cs
@@ -0,0 +1,21 @@
+﻿using AlienEngine.Core.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienEngine
+{
+    public static class Application
+    {
+        /// <summary>
+        /// Stop the <see cref="Game"/>, dispose all
+        /// resources and close the application.
+        /// </summary>
+        public static void Quit()
+        {
+
+        }
+    }
+}
