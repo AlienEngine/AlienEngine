4efd5c7085ab025def047538efc204415dc62607
Nana Axel <ax.lnana@outlook.com>
2017-09-16 01:44:13 +0100

    Add bounding volume structures

---
+++ AlienEngine/Math/BoundingSphere.cs
@@ -0,0 +1,28 @@
+﻿namespace AlienEngine
+{
+    /// <summary>
+    /// Manage bounding sphere volumes.
+    /// </summary>
+    public struct BoundingSphere
+    {
+        /// <summary>
+        /// Radius of the sphere.
+        /// </summary>
+        public float Radius;
+        /// <summary>
+        /// Location of the center of the sphere.
+        /// </summary>
+        public Vector3f Center;
+
+        /// <summary>
+        /// Constructs a new <see cref="BoundingSphere"/>.
+        /// </summary>
+        /// <param name="center">Location of the center of the sphere.</param>
+        /// <param name="radius">Radius of the sphere.</param>
+        public BoundingSphere(Vector3f center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+    }
+}
---
+++ AlienEngine/Math/ContainmentType.cs
@@ -0,0 +1,23 @@
+﻿namespace AlienEngine
+{
+    /// <summary>
+    /// The current containment state of two objects.
+    /// </summary>
+    public enum ContainmentType
+    {
+        /// <summary>
+        /// The objects are separate.
+        /// </summary>
+        Disjoint,
+
+        /// <summary>
+        /// One object fully contains the other.
+        /// </summary>
+        Contains,
+
+        /// <summary>
+        /// The objects are intersecting, but neither object fully contains the other.
+        /// </summary>
+        Intersects
+    }
+}
