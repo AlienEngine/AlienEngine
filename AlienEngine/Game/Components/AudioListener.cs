472ce17f14852d9be02237044ebefa608624b402
Nana Axel <ax.lnana@outlook.com>
2017-09-16 01:37:21 +0100

    Update AudioListener

--- AlienEngine/Game/Components/AudioListener.cs
+++ AlienEngine/Game/Components/AudioListener.cs
@@ -4,32 +4,19 @@ namespace AlienEngine
 {
     public class AudioListener : Component
     {
-        /// <summary>
-        /// The velociy of the sound listener.
-        /// </summary>
-        private Vector3f _velocity;
-
-        /// <summary>
-        /// EFX meters per unit.
-        /// </summary>
-        private float _efxMetersPerUnit;
-
-        /// <summary>
-        /// The gain (volume).
-        /// </summary>
-        private float _gain;
+        private Vector3f _lastPos;
 
         /// <summary>
         /// Constructor.
         /// </summary>
-        public AudioListener() : this(Vector3f.Zero, 0.5f, 1f)
+        public AudioListener() : this(Vector3f.Zero, 1f, 1f)
         { }
 
         public AudioListener(Vector3f velocity, float efxMetersPerUnit, float gain)
         {
-            _velocity = velocity;
-            _efxMetersPerUnit = efxMetersPerUnit;
-            _gain = gain;
+            AL.Listener(ALListener3f.Velocity, ref velocity);
+            AL.Listener(ALListenerf.EfxMetersPerUnit, efxMetersPerUnit);
+            AL.Listener(ALListenerf.Gain, gain);
         }
 
         /// <summary>
@@ -38,7 +25,12 @@ namespace AlienEngine
         /// <returns>The <see cref="Vector3f"/> of the velocity.</returns>
         public Vector3f Velocity
         {
-            get { return _velocity; }
+            get
+            {
+                Vector3f v;
+                AL.GetListener(ALListener3f.Velocity, out v);
+                return v;
+            }
         }
 
         /// <summary>
@@ -47,7 +39,12 @@ namespace AlienEngine
         /// <returns>The EFX meters per unit.</returns>
         public float EfxMetersPerUnit
         {
-            get { return _efxMetersPerUnit; }
+            get
+            {
+                float m;
+                AL.GetListener(ALListenerf.EfxMetersPerUnit, out m);
+                return m;
+            }
         }
 
         /// <summary>
@@ -56,33 +53,48 @@ namespace AlienEngine
         /// <returns>The gain.</returns>
         public float Gain
         {
-            get { return _gain; }
+            get
+            {
+                float g;
+                AL.GetListener(ALListenerf.EfxMetersPerUnit, out g);
+                return g;
+            }
         }
 
         public override void Start()
         {
             MakeCurrent();
 
+            Transform defT = gameElement.WorldTransform;
+            Vector3f defPos = defT.Translation, defAt = defT.ForwardVector, defUp = defT.UpVector;
+
+            AL.Listener(ALListener3f.Position, ref defPos);
+            AL.Listener(ALListenerfv.Orientation, ref defAt, ref defUp);
+
             gameElement.LocalTransform.OnPositionChange += ((_old, _new) =>
             {
-                Vector3f _delta = _new - _old;
-                AL.Listener(ALListener3f.Position, ref _new);
-                AL.Listener(ALListener3f.Velocity, ref _delta);
+                Vector3f pos = gameElement.WorldTransform.Translation;
+                AL.Listener(ALListener3f.Position, ref pos);
             });
 
             gameElement.LocalTransform.OnRotationChange += ((_old, _new) =>
             {
-                Vector3f _forward = gameElement.LocalTransform.ForwardVector,
-                         _up = gameElement.LocalTransform.UpVector;
+                Vector3f _forward = gameElement.WorldTransform.ForwardVector,
+                         _up = gameElement.WorldTransform.UpVector;
 
                 AL.Listener(ALListenerfv.Orientation, ref _forward, ref _up);
             });
+
+            _lastPos = defPos;
         }
 
         public override void Update()
         {
-            // Reset Velocity
-            AL.Listener(ALListener3f.Velocity, Vector3f.Zero.X, Vector3f.Zero.Y, Vector3f.Zero.Z);
+            // Update Velocity
+            Vector3f _newPos = gameElement.WorldTransform.Translation;
+            Vector3f vel = (_newPos - _lastPos);
+            AL.Listener(ALListener3f.Velocity, ref vel);
+            _lastPos = _newPos;
         }
 
         /// <summary>
@@ -100,31 +112,20 @@ namespace AlienEngine
         }
 
         /// <summary>
-        /// Changes the velocity of this listener.
-        /// </summary>
-        /// <param name="orientation">The new velocity.</param>
-        public void SetVelocity(Vector3f velocity)
-        {
-            _velocity = velocity;
-        }
-
-        /// <summary>
         /// Changes the EFX meters per unit of this listener.
         /// </summary>
-        /// <param name="orientation">The new EFX meters per unit.</param>
+        /// <param name="efxMetersPerUnit">The new EFX meters per unit.</param>
         public void SetEfxMetersPerUnit(float efxMetersPerUnit)
         {
-            _efxMetersPerUnit = efxMetersPerUnit;
             AL.Listener(ALListenerf.EfxMetersPerUnit, EfxMetersPerUnit);
         }
 
         /// <summary>
         /// Changes the gain of this listener.
         /// </summary>
-        /// <param name="orientation">The new gain.</param>
+        /// <param name="gain">The new gain.</param>
         public void SetGain(float gain)
         {
-            _gain = gain;
             AL.Listener(ALListenerf.Gain, Gain);
         }
     }
