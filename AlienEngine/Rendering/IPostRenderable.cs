c4387dd45090202dc3abc226e9f91fb766b5337d
Axel Nana <ax.lnana@outlook.com>
2017-10-29 20:58:27 +0100

    Rename IRenderableText to IPostRenderable

---
+++ AlienEngine/Rendering/IPostRenderable.cs
@@ -0,0 +1,9 @@
+﻿namespace AlienEngine.Core.Rendering
+{
+    /// <summary>
+    /// Interface providing methods for post-state renderable objects.
+    /// </summary>
+    public interface IPostRenderable: IRenderable
+    {
+    }
+}
