dcb767e4d5e86b13cd99048141cfc71dffcf845e
Axel Nana <ax.lnana@outlook.com>
2017-10-29 14:22:42 +0100

    Update Monitor manager

--- AlienEngine/Graphics/Monitor.cs
+++ AlienEngine/Graphics/Monitor.cs
@@ -2,34 +2,22 @@
 
 namespace AlienEngine.Core.Graphics
 {
+    /// <summary>
+    /// Handle and manage monitors.
+    /// </summary>
     public class Monitor
     {
-        #region Static members
-        /// <summary>
-        /// Gets a NULL monitor's pointer.
-        /// </summary>
-        public static Monitor None
-        {
-            get { return new Monitor(GLFWMonitor.None); }
-        }
-
-        /// <summary>
-        /// Gets the primary monitor.
-        /// </summary>
-        public static Monitor PrimaryMonitor
-        {
-            get { return new Monitor(GLFW.GLFW.GetPrimaryMonitor()); }
-        }
-        #endregion
-
         #region Fields
+
         /// <summary>
         /// The internal monitor's pointer.
         /// </summary>
         internal readonly GLFWMonitor Handle;
+
         #endregion
 
         #region Private members
+
         /// <summary>
         /// Initialize a new <see cref="Monitor"/> instance with the given
         /// <see cref="Handle"/>.
@@ -39,6 +27,59 @@ namespace AlienEngine.Core.Graphics
         {
             Handle = handle;
         }
+
+        #endregion
+
+        #region Public members
+
+        /// <summary>
+        /// Gets the name of this <see cref="Monitor"/>.
+        /// </summary>
+        public string Name => GLFW.GLFW.GetMonitorName(Handle);
+
+        #endregion
+
+        #region Static members
+
+        /// <summary>
+        /// Gets a NULL monitor's pointer.
+        /// </summary>
+        public static Monitor None => new Monitor(GLFWMonitor.None);
+
+        /// <summary>
+        /// Gets the primary monitor.
+        /// </summary>
+        public static Monitor PrimaryMonitor => new Monitor(GLFW.GLFW.GetPrimaryMonitor());
+
+        /// <summary>
+        /// Gets the total number of monitors.
+        /// </summary>
+        public static int MonitorsCount
+        {
+            get
+            {
+                var monitors = GetMonitors();
+                return monitors.Length;
+            }
+        }
+
+        /// <summary>
+        /// Get all monitors connected.
+        /// </summary>
+        /// <returns>An array of <see cref="Monitor"/>.</returns>
+        public static Monitor[] GetMonitors()
+        {
+            var glfwMonitors = GLFW.GLFW.GetMonitors();
+            var monitors = new Monitor[glfwMonitors.Length];
+
+            for (int i = 0, l = glfwMonitors.Length; i < l; i++)
+            {
+                monitors[i] = new Monitor(glfwMonitors[i]);
+            }
+
+            return monitors;
+        }
+
         #endregion
     }
 }
