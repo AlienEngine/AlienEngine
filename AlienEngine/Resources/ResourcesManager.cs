25c55cbcccf85638b8369bc1a45f19f6a0fa3211
Axel Nana <ax.lnana@outlook.com>
2017-11-12 18:18:18 +0100

    Little loop optimization

--- AlienEngine/Resources/ResourcesManager.cs
+++ AlienEngine/Resources/ResourcesManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlienEngine.Core.Resources
 {
@@ -52,10 +49,11 @@ namespace AlienEngine.Core.Resources
             OnDisposeResources();
             OnDisposeResources = null;
 
-            for (int i = 0; i < _disposableResources.Count; i++)
+            for (int i = 0, l = _disposableResources.Count; i < l; i++)
                 if (_disposableResources[i] != null)
                     _disposableResources[i].Dispose();
 
+            _disposableResources.Clear();
             _disposableResources = null;
             _disposed = true;
         }
