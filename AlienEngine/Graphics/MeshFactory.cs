d04cd19895ce5fd8afa765fa9b745327880fdca6
Axel Nana <ax.lnana@outlook.com>
2017-11-12 18:09:25 +0100

    Fix buffer creation

--- AlienEngine/Graphics/MeshFactory.cs
+++ AlienEngine/Graphics/MeshFactory.cs
@@ -86,8 +86,8 @@ namespace AlienEngine.Core.Graphics
             GL.VertexAttribPointer(GL.VERTEX_POSITION_LOCATION, vertex.Size, vertex.PointerType, false, 0, 0);
 
             GL.BindBuffer(texture.BufferTarget, texture.ID);
-            GL.EnableVertexAttribArray(GL.VERTEX_NORMAL_LOCATION);
-            GL.VertexAttribPointer(GL.VERTEX_NORMAL_LOCATION, texture.Size, texture.PointerType, false, 0, 0);
+            GL.EnableVertexAttribArray(GL.VERTEX_TEXTURE_COORD_LOCATION);
+            GL.VertexAttribPointer(GL.VERTEX_TEXTURE_COORD_LOCATION, texture.Size, texture.PointerType, false, 0, 0);
 
             GL.BindBuffer(normal.BufferTarget, normal.ID);
             GL.EnableVertexAttribArray(GL.VERTEX_NORMAL_LOCATION);
@@ -99,9 +99,71 @@ namespace AlienEngine.Core.Graphics
             GL.BindVertexArray(0);
 
             // Add a dispose event to delete the generated VAO
-            Resources.ResourcesManager.AddOnDisposeEvent(() => GL.DeleteVertexArrays(1, _vao));
+            Resources.ResourcesManager.AddOnDisposeEvent(() => GL.DeleteVertexArray(_vao));
 
             return new Mesh(_vao, plane);
+        }
+
+        /// <summary>
+        /// Create a basic quad by storing two triangles into a VAO.
+        /// This quad includes UV co-ordinates from 0,0 to 1,1.
+        /// </summary>
+        /// <param name="program">The ShaderProgram assigned to this quad.</param>
+        /// <param name="location">The location of the VAO (assigned to the vertices).</param>
+        /// <param name="size">The size of the VAO (assigned to the vertices).</param>
+        /// <returns>The VAO object representing this quad.</returns>
+        public static Mesh CreateQuad(Point2f location, Sizef size, Point2f uvloc, Sizef uvsize)
+        {
+            Vector3f[] vertices = new Vector3f[]
+            {
+                new Vector3f(location.X, location.Y, 0),
+                new Vector3f(location.X + size.X, location.Y, 0),
+                new Vector3f(location.X + size.X, location.Y + size.Y, 0),
+                new Vector3f(location.X, location.Y + size.Y, 0)
+            };
+
+            Vector2f[] uvs = new Vector2f[]
+            {
+                new Vector2f(uvloc.X, uvloc.Y),
+                new Vector2f(uvloc.X + uvsize.X, uvloc.Y),
+                new Vector2f(uvloc.X + uvsize.X, uvloc.Y + uvsize.Y),
+                new Vector2f(uvloc.X, uvloc.Y + uvsize.Y)
+            };
+
+            int[] indices = new int[] { 0, 1, 2, 2, 3, 0 };
+
+            MeshEntry quad = new MeshEntry();
+
+            quad.BaseIndex = 0;
+            quad.BaseVertex = 0;
+            quad.NumIndices = indices.Length;
+            quad.Name = "Quad";
+
+            // Create the VAO
+            uint _vao = GL.GenVertexArray();
+            GL.BindVertexArray(_vao);
+
+            VBO<Vector3f> vertex = new VBO<Vector3f>(vertices);
+            VBO<Vector2f> texture = new VBO<Vector2f>(uvs);
+            VBO<int> element = new VBO<int>(indices, BufferTarget.ElementArrayBuffer, BufferUsageHint.StaticRead);
+
+            GL.BindBuffer(vertex.BufferTarget, vertex.ID);
+            GL.EnableVertexAttribArray(GL.VERTEX_POSITION_LOCATION);
+            GL.VertexAttribPointer(GL.VERTEX_POSITION_LOCATION, vertex.Size, vertex.PointerType, false, 0, 0);
+
+            GL.BindBuffer(texture.BufferTarget, texture.ID);
+            GL.EnableVertexAttribArray(GL.VERTEX_TEXTURE_COORD_LOCATION);
+            GL.VertexAttribPointer(GL.VERTEX_TEXTURE_COORD_LOCATION, texture.Size, texture.PointerType, false, 0, 0);
+
+            GL.BindBuffer(element.BufferTarget, element.ID);
+
+            // Make sure this VAO is not modified from the outside
+            GL.BindVertexArray(0);
+
+            // Add a dispose event to delete the generated VAO
+            Resources.ResourcesManager.AddOnDisposeEvent(() => GL.DeleteVertexArray(_vao));
+
+            return new Mesh(_vao, quad);
         }
 
         /// <summary>
@@ -165,7 +227,7 @@ namespace AlienEngine.Core.Graphics
             GL.BindVertexArray(0);
 
             // Add a dispose event to delete the generated VAO
-            Resources.ResourcesManager.AddOnDisposeEvent(() => GL.DeleteVertexArrays(1, _vao));
+            Resources.ResourcesManager.AddOnDisposeEvent(() => GL.DeleteVertexArray(_vao));
 
             return new Mesh(_vao, cube);
         }
