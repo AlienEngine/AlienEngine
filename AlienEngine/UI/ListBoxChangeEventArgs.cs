2ef4634479c764d1526ed6a0d9d7df81965cda51
Axel Nana <ax.lnana@outlook.com>
2017-11-13 23:37:22 +0100

    Add ListBoxChangeEventArgs

---
+++ AlienEngine/UI/ListBoxChangeEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AlienEngine.UI
+{
+    /// <summary>
+    /// Stores data about a change event in a list box.
+    /// </summary>
+    public class ListBoxChangeEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The last value just before the change.
+        /// </summary>
+        public readonly string OldValue;
+
+        /// <summary>
+        /// The new value.
+        /// </summary>
+        public readonly string NewValue;
+
+        /// <summary>
+        /// Constructs a new list box change event data store.
+        /// </summary>
+        /// <param name="oldValue">The old value.</param>
+        /// <param name="newValue">The new value.</param>
+        public ListBoxChangeEventArgs(string oldValue, string newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
