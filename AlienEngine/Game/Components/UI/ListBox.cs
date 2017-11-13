2074f8833e297a2b1049de23bdd298e9f13589bb
Axel Nana <ax.lnana@outlook.com>
2017-11-13 09:15:41 +0100

    Add a ListBox UI element

---
+++ AlienEngine/Game/Components/UI/ListBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienEngine
+{
+    public class ListBox: UIComponent
+    {
+        private List<Label2D> _lines;
+
+        public ListBox()
+        {
+            _lines = new List<Label2D>();
+        }
+    }
+}
