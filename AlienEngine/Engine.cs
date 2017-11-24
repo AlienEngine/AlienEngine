184075e4470087a92cb3c53be0e3fc6aaad237dd
Axel Nana <ax.lnana@outlook.com>
2017-11-24 10:21:21 +0100

    Update engine for threading

--- AlienEngine/Engine.cs
+++ AlienEngine/Engine.cs
@@ -1,8 +1,11 @@
-﻿using AlienEngine.Core.Audio;
+﻿using System.Collections.Generic;
+using AlienEngine.Core.Audio;
 using AlienEngine.Core.Audio.OpenAL;
 using AlienEngine.Core.Graphics.DevIL.Unmanaged;
 using AlienEngine.Core.Graphics.OpenGL;
+using AlienEngine.Core.Physics.BroadPhaseEntries;
 using AlienEngine.Core.Resources;
+using AlienEngine.Core.Threading;
 
 namespace AlienEngine.Core
 {
@@ -14,13 +17,18 @@ namespace AlienEngine.Core
         private static bool _started;
         private static AudioContext _audioContext;
 
+        private static List<Task> _physicsTasks;
+        private static List<Task> _renderingTasks;
+        private static List<Task> _audioTasks;
+
+        private static Worker _physicsWorker;
+        private static Worker _renderingWorker;
+        private static Worker _audioWorker;
+        
         /// <summary>
         /// Checks if the engine has already started.
         /// </summary>
-        public static bool HasStarted
-        {
-            get { return _started; }
-        }
+        public static bool HasStarted => _started;
 
         /// <summary>
         /// Initialize the manager.
@@ -28,6 +36,14 @@ namespace AlienEngine.Core
         static Engine()
         {
             _started = false;
+            
+            _renderingTasks = new List<Task>();
+            _physicsTasks = new List<Task>();
+            _audioTasks = new List<Task>();
+            
+            _renderingWorker = new Worker(new WorkStealingScheduler(1), (int)WorkerItemIndex.Renderer);
+            _physicsWorker = new Worker(new WorkStealingScheduler(1), (int)WorkerItemIndex.Physics);
+            _audioWorker = new Worker(new WorkStealingScheduler(1), (int)WorkerItemIndex.Audio);
         }
 
         /// <summary>
