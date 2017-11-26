d1e6db394cb68fd7e5a4d4b91ed33dbb6e333b84
Axel Nana <ax.lnana@outlook.com>
2017-11-26 20:16:11 +0100

    Basic implementation

--- AlienEngine/IO/FileSystemManager.cs
+++ AlienEngine/IO/FileSystemManager.cs
@@ -1,6 +1,30 @@
-﻿namespace AlienEngine.Core.IO
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AlienEngine.Core.IO
 {
     public static class FileSystemManager
     {
+
+        public static string ReadFileAsString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static byte[] ReadFileAsBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        public static FileStream Read(string path)
+        {
+            return File.OpenRead(path);
+        }
+
+        public static async Task<string> AsyncReadFileAsString(string path)
+        {
+            return await Task.Run(() => ReadFileAsString(path));
+        }
+
     }
 }
