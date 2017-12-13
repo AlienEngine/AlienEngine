17a9b3a0b2bd288be5f4806d87d3ba2deef83e0b
Axel Nana <ax.lnana@outlook.com>
2017-12-14 00:03:46 +0100

    Update game assets. Add MaterialAsset, TextureAsset and update MeshAsset

---
+++ AlienEngine/Assets/Material/ShaderData.cs
@@ -0,0 +1,26 @@
+﻿using ZeroFormatter;
+
+namespace AlienEngine.Core.Assets.Material
+{
+    [ZeroFormattable]
+    public class ShaderData
+    {
+        [Index(0)]
+        public virtual string VertexShader { get; set; }
+
+        [Index(1)]
+        public virtual string FragmentShader { get; set; }
+
+        [Index(2)]
+        public virtual string GeometryShader { get; set; }
+
+        [Index(3)]
+        public virtual string TesselationControlShader { get; set; }
+
+        [Index(4)]
+        public virtual string TesselationEvaluationShader { get; set; }
+        
+        public ShaderData()
+        { }
+    }
+}
---
+++ AlienEngine/Assets/Texture/TextureWrapMode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienEngine.Core.Assets.Texture
+{
+    public enum TextureWrapMode
+    {
+    }
+}
