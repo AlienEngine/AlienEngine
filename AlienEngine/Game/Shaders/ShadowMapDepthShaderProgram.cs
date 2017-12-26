3c0262de8ab729af5a0a6d0beb0bee28a7083009
Axel Nana <ax.lnana@outlook.com>
2017-12-26 11:59:02 +0100

    Add shadow map shaders

---
+++ AlienEngine/Game/Shaders/ShadowMapDepthShaderProgram.cs
@@ -0,0 +1,11 @@
+﻿using AlienEngine.Core.Shaders;
+using AlienEngine.Core.Shaders.Samples;
+
+namespace AlienEngine.Shaders
+{
+    internal class ShadowMapDepthShaderProgram: ShaderProgram
+    {
+        public ShadowMapDepthShaderProgram(): base(new ShadowMapDepthVertexShader(), new ShadowMapDepthFragmentShader())
+        { }
+    }
+}
---
+++ AlienEngine/Shaders/Samples/Fragment Shaders/ShadowMapDepthFragmentShader.cs
@@ -0,0 +1,15 @@
+﻿using AlienEngine.ASL;
+
+namespace AlienEngine.Core.Shaders.Samples
+{
+    [Version("330 core")]
+    public class ShadowMapDepthFragmentShader: FragmentShader
+    {
+        [Out] vec4 FragColor;
+
+        void main()
+        {
+            FragColor = new vec4(0, 0, 0, 1);
+        }
+    }
+}
