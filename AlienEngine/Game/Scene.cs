500688a79dcac6dbdbd489f455cc3f1c8ea7086d
Nana Axel <ax.lnana@outlook.com>
2018-01-19 00:20:55 +0100

    Comment code

--- AlienEngine/Game/Scene.cs
+++ AlienEngine/Game/Scene.cs
@@ -232,12 +232,18 @@ namespace AlienEngine.Core.Game
         public virtual void Unload()
         { }
 
+        /// <summary>
+        /// Starts the <see cref="Scene"/>.
+        /// </summary>
         public virtual void Start()
         {
             foreach (GameElement element in _gameElements)
                 element.Start();
         }
 
+        /// <summary>
+        /// Triggered before the frame's update of the <see cref="Scene"/>.
+        /// </summary>
         public virtual void BeforeUpdate()
         {
             foreach (GameElement element in _gameElements)
@@ -253,6 +259,9 @@ namespace AlienEngine.Core.Game
                 element.Update();
         }
 
+        /// <summary>
+        /// Triggered after the frame's update of the <see cref="Scene"/>.
+        /// </summary>
         public virtual void AfterUpdate()
         {
             foreach (GameElement element in _gameElements)
