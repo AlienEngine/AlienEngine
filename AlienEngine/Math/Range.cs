a0af3187b57fc49df3f06f8317f7fefc58c329e9
Nana Axel <ax.lnana@outlook.com>
2018-01-22 01:33:25 +0100

    Create the struct Range

---
+++ AlienEngine/Math/Range.cs
@@ -0,0 +1,25 @@
+﻿namespace AlienEngine
+{
+    public struct Range
+    {
+        private int _start;
+
+        private int _end;
+
+        public int Start => _start;
+
+        public int End => _end;
+
+        public int Length
+        {
+            get { return _end - _start; }
+            set { _end = _start + value; }
+        }
+
+        public Range(int start, int end)
+        {
+            _start = start;
+            _end = end;
+        }
+    }
+}
