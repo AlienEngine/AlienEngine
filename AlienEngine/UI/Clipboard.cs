6bca17a56bab48a4f4db15eda3b74c10eeb725fb
Nana Axel <ax.lnana@outlook.com>
2018-01-22 01:34:34 +0100

    Create the class Clipboard
    The Clipboard class is used to manage the clipboard in the game runtime.

---
+++ AlienEngine/UI/Clipboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace AlienEngine.UI
+{
+    public static class Clipboard
+    {
+        private static readonly Stack<string> Clips;
+
+        public static Stack<string> History => Clips;
+
+        static Clipboard()
+        {
+            Clips = new Stack<string>();
+        }
+
+        public static void Copy(string text)
+        {
+            Clips.Push(text);
+        }
+
+        public static string Paste()
+        {
+            return Clips.Count > 0 ? Clips.Peek() : "";
+        }
+
+        public static string PasteRemove()
+        {
+            return Clips.Count > 0 ? Clips.Pop() : "";
+        }
+    }
+}
