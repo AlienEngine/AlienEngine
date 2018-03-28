f7070998b5ccbe71e343c06e9de0d078a761ffa0
Nana Axel <ax.lnana@outlook.com>
2018-03-28 03:52:50 +0100

    Update IResource

--- AlienEngine/Resources/IResource.cs
+++ AlienEngine/Resources/IResource.cs
@@ -1,7 +1,18 @@
-﻿namespace AlienEngine.Core.Resources
+﻿using System;
+using AlienEngine.Core.Assets;
+
+namespace AlienEngine.Core.Resources
 {
-    public interface IResource
+    /// <summary>
+    /// Represents a game resource.
+    /// </summary>
+    public interface IResource: IDisposable
     {
+        /// <summary>
+        /// Loads the resource from a path.
+        /// </summary>
+        /// <param name="path">The path to the resource.</param>
+        /// <returns><value>true</value> if the resource was successfuly loaded, <value>false</value> otherwise.</returns>
         bool Load(string path);
     }
 }
